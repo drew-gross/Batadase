dbe2a6bd862864bd9a32ae0d036e7df69786439b
Drew Gross <drew.a.gross@gmail.com>
2012-05-05 04:38:06 -0400

    changes

--- server_resart.sh
+++ server_resart.sh
@@ -12,6 +12,6 @@ find . -iname '*.pyc' -exec rm {} \;
 
 kill `cat $PIDFILE`
 
-./manage.py runfcgi pidfile=$PIDFILE debug=True outlog=$OUTLOG errlog=$ERRLOG
+./Batadase/manage.py runfcgi pidfile=$PIDFILE debug=True outlog=$OUTLOG errlog=$ERRLOG
 
 sudo service nginx reload
