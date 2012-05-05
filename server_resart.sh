a99ca58269db69e98a2c6d44937d1ba2cd83739a
Drew Gross <drew.a.gross@gmail.com>
2012-05-05 04:37:24 -0400

    changes

--- server_resart.sh
+++ server_resart.sh
@@ -3,7 +3,6 @@
 ROOT_PATH="~/"
 
 PIDFILE="${ROOT_PATH}/django-flup.pid"
-SOCKETFILE="${ROOT_PATH}/django.sock"
 OUTLOG="${ROOT_PATH}/django.out"
 ERRLOG="${ROOT_PATH}/django.err"
 
@@ -13,6 +12,6 @@ find . -iname '*.pyc' -exec rm {} \;
 
 kill `cat $PIDFILE`
 
-sg croutonlabs_web "./manage.py runfcgi socket=$SOCKETFILE pidfile=$PIDFILE umask=0002 debug=True outlog=$OUTLOG errlog=$ERRLOG"
+./manage.py runfcgi pidfile=$PIDFILE debug=True outlog=$OUTLOG errlog=$ERRLOG
 
 sudo service nginx reload
