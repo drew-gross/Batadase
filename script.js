6cc1829b8585c9256a76e4bcacf0a5e7e1169c8f
root <root@ip-10-195-221-182.ec2.internal>
2012-05-07 00:23:25 +0000

    we need an object

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 batadase = (function(){
+    var batadase = new Object();
     batadase.get = function(key) {
       http = new XMLHttpRequest();
       http.open("GET", "http://batadase.com/" + key);
